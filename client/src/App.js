102d159365d6e70b436bc3fa3507e67005b76aa2
Tarek Schütz <tarekschutz@Tareks-MacBook-Air.local>
2023-11-29 10:20:42 +0100

    added login and register, css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Axios from 'axios';
+
 function App() {
-  const [password, setPassword] = useState('')
-  const [title, setTitle] = useState('')
+  const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+  const [passwordList, setPasswordList] = useState([]);
+  const [username, setUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/getpasswords').then((response) => {
+      setPasswordList(response.data)
+    });
+  }, []);
 
   const addPassword = () => {
     Axios.post('http://localhost:3001/addpassword', {
@@ -11,27 +24,106 @@ function App() {
       title: title
     });
   };
+
+  const decryptPassword = (encryption) => {
+    Axios.post('http://localhost:3001/decryptpassword', {
+      password: encryption.password,
+      iv: encryption.iv,
+    }).then((response) => {
+      setPasswordList(passwordList.map((val) => {
+        return val.id == encryption.id ? {
+          id: val.id,
+          password: val.password,
+          title: response.data,
+          iv: val.iv
+        }
+        : val;
+      }))
+    })
+  };
+
+  const login = () => {
+    Axios.post('http://localhost:3001/login', {
+      username: username, 
+      password: loginPassword
+    }).then((response) => {
+      if (response.data.loggedIn) {
+        setLoggedIn(true);
+      }
+    });
+  };
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register', {
+      username: registerUsername, 
+      password: registerPassword
+    }).then((response) => {
+      if (response.data === "Success") {
+        alert("User registered successfully");
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <div className="AddingPassword">
-        <input 
-          type="text" 
-          placeholder="Ex. password123"
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <input 
-          type="text" 
-          placeholder="Ex. MySpace"
-          onChange={(event) => {
-            setTitle(event.target.value);
-            }}
-        />
-        <button onClick={addPassword}>Add Password</button>
-      </div>
+      {!loggedIn ? (
+        <div>
+          <div>
+            <h2>Register</h2>
+            <input type="text" placeholder="Username" onChange={(e) => setRegisterUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setRegisterPassword(e.target.value)} />
+            <button onClick={register}>Register</button>
+          </div>
+          <div>
+            <h2>Login</h2>
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)} />
+            <button onClick={login}>Login</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="AddingPassword">
+            <input 
+                type="text" 
+                placeholder="Ex. MySpace"
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                  }}
+              />
+              <input 
+                type="text" 
+                placeholder="Ex. password123"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              <button onClick={addPassword}>Add Password</button>
+            </div>
+
+            <div className="displayPasswords">
+              {passwordList.map((val, key) => {
+                return (
+                  <div
+                   className="password" 
+                   onClick = {() => {
+                    decryptPassword({
+                      password: val.password, 
+                      iv: val.iv,
+                      id: val.id,
+                    })
+                    key={key}
+                   }}>
+                    <h3>{val.title}</h3>
+                  </div>
+                );
+              })}
+            </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 const PORT = 3001;
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'your-secret-key'; // Replace with your own secret key
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const {encrypt, decrypt} = require('./EncryptionHandler');
 
 app.use(cors());
 app.use(express.json());
@@ -10,16 +17,90 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: '153624',
-    databse: 'PasswordManager'
+    password: 'passwort',
+    database: 'passwortmanager'
 });
 
-app.post("/addpassword", (req, res) => {
-    const { password, title } = req.body;
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send({ message: "An error occurred" });
+            return;
+        }
+
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            [username, hash],
+            (error, result) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).send({ message: "An error occurred" });
+                } else {
+                    res.send("Success");
+                }
+            }
+        );
+    });
+});
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
 
     db.query(
-        "INSERT INTO passwords (password, title) VALUES (?, ?)", 
-        [password, title],
+        "SELECT * FROM users WHERE username = ?",
+        username,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: "An error occurred" });
+                return;
+            }
+
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).send({ message: "An error occurred" });
+                        return;
+                    }
+
+                    if (response) {
+                        const token = jwt.sign({ id: result[0].id, username: result[0].username }, SECRET_KEY);
+                        res.send({ loggedIn: true, user: { username: result[0].username, id: result[0].id }, token });
+                    } else {
+                        res.send({ loggedIn: false, message: "Wrong username/password combination!" });
+                    }
+                });
+            } else {
+                res.send({ loggedIn: false, message: "User doesn't exist" });
+            }
+        }
+    );
+});
+
+function verifyToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.post("/addpassword", verifyToken, (req, res) => {
+    const { password, title } = req.body;
+    const userId = req.user.id;
+    const hashedPassword = encrypt(password);
+    db.query(
+        "INSERT INTO passwords (password, title, iv, user_id) VALUES (?, ?, ?, ?)", 
+        [hashedPassword.password, title, hashedPassword.iv, userId],
         (err, result) => {
             if (err) {
                 console.log(err);
@@ -28,6 +109,21 @@ app.post("/addpassword", (req, res) => {
             }
         }
     );
+});
+
+app.get("/getpasswords", verifyToken, (req, res) => {
+    const userId = req.user.id;
+    db.query("SELECT * FROM passwords WHERE user_id = ?", [userId], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+});
+
+app.post("/decryptpassword", (req, res) => {
+    res.send(decrypt(req.body));
 });
 
 app.listen(PORT, () => {
